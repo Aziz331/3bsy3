201a857dc731d1ce2db21eebbefa88c7b0e6a3ea
Aziz331 <44896829+Aziz331@users.noreply.github.com>
2018-11-22 00:58:28 +0700

    Update bot.js.js

--- bot.js.js
+++ bot.js.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "514858579002392589"; // ايدي السررفر
+var channel = "514858579426279443";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
